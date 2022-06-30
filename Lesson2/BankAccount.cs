a2004f5f1b766d56447c27e749acd633924d0a98
Nikita <Smerdix@mail.ru>
2022-06-30 21:03:19 +0400

    Рефакторинг

---
+++ Lesson2/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿//Для физических лиц
+public enum TypeBankAccount
+{
+    Current,
+    Accounting,
+    Credit,
+    Deposit,
+}
+
+public class BankAccount
+{
+
+    private static int accountNumber = 1;
+
+    private int _number;
+
+    public decimal Balance { get; set; }
+
+    public TypeBankAccount Type { get; set; }
+
+    public int Number
+    {
+        get => _number;
+        set
+        {
+            _number = value;
+        }
+    }
+
+    public BankAccount(decimal balance)
+    {
+        Balance = balance;
+        Number = GenerateAccount();
+    }
+
+    public BankAccount(TypeBankAccount type)
+    {
+        Type = type;
+        Number = GenerateAccount();
+    }
+
+    public BankAccount(decimal balance, TypeBankAccount type)
+    {
+        Balance = balance;
+        Type = type;
+        Number = GenerateAccount();
+    }
+
+    private static int GenerateAccount()
+    {
+        return accountNumber++;
+    }
+
+    public void GetAccountNumber()
+    {
+
+        Console.WriteLine($"Информация о счёте: номер счёта - {accountNumber}; баланс - {Balance} руб.; тип счёта - {Type}");
+        accountNumber++;
+        Console.WriteLine();
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Первая реализация, через List
+var ListAccount = new List<BankAccount>();
+ListAccount.Add(new BankAccount(546546567));
+ListAccount.Add(new BankAccount(TypeBankAccount.Credit));
+ListAccount.Add(new BankAccount(32434, TypeBankAccount.Credit));
+
+for (int i = 0; i < 3; i++)
+{
+    GetAccount(ListAccount[i]);
+    Console.WriteLine();
+}
+
+void GetAccount(BankAccount account)
+{
+    Console.WriteLine($"Счет: {account.Number}");
+    Console.WriteLine($"Тип счета: {account.Type}");
+    Console.WriteLine($"Баланс счета: {account.Balance}");
+    Console.WriteLine();
+}
+
+
+//Вторая реализация
+BankAccount[] accountNumbers = new BankAccount[5];
+accountNumbers[0] = new(343454, TypeBankAccount.Credit);
+accountNumbers[1] = new(546456, TypeBankAccount.Accounting);
+accountNumbers[2] = new(3244, TypeBankAccount.Credit);
+accountNumbers[3] = new(23, TypeBankAccount.Current);
+accountNumbers[4] = new(2342342344, TypeBankAccount.Deposit);
+
+foreach (BankAccount bankAccount in accountNumbers)
+{
+    bankAccount.GetAccountNumber();
+}
+
+Console.ReadKey(true);
