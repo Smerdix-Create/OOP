90228e071fccc9d73c36d0717f5777a6e71ca615
Nikita <Smerdix@mail.ru>
2022-07-07 11:07:40 +0400

    Реализация класса для описания здания

---
+++ Lesson4/Building/BuildingInfo.cs
@@ -0,0 +1,60 @@
+﻿namespace Lesson4.Building
+{
+    public class BuildingInfo
+    {
+        private static int numberBuilding = 1;
+       
+        public double Height { get; private set; }
+       
+        public int Floor { get; }
+        
+        //Кол-во квартир на этаже
+        public int Flats { get; }
+        
+        //Кол-во подъездов в доме
+        public int Entrance { get; }
+        
+        public int EntranceFlats { get; private set; }
+
+        public int EntrancesFlats { get; private set; }
+
+        public int Number { get; private set; }
+
+        public BuildingInfo(int floor, int flats, int entrance)
+        {
+            Number = GetNumberBuilding();
+            this.Floor = floor;
+            this.Flats = flats;
+            this.Entrance = entrance;
+        }
+
+        private static int GetNumberBuilding()
+        {
+            return numberBuilding++;
+        }
+
+        /// <summary>
+        /// Получить высоту здания
+        /// </summary>
+        public void GetHeight()
+        {
+            Height = Floor * 2.9 + 1.5 + 2;
+        }
+
+        /// <summary>
+        /// Получить кол-во квартир в 1 подъезде
+        /// </summary>
+        public void GetEntranceFlats()
+        {
+            EntranceFlats = Flats * Floor;
+        }
+
+        /// <summary>
+        /// Получить кол-во квартир во всём доме
+        /// </summary>
+        public void GetEntrancesFlats()
+        {
+            EntrancesFlats = EntranceFlats * Entrance;
+        }
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lesson4.Building;
+
+var list = new List<BuildingInfo>()
+{
+    new BuildingInfo(4, 4, 4),
+    new BuildingInfo(5, 5, 5),
+    new BuildingInfo(1, 2, 3)
+};
+
+foreach (var buildingInfo in list)
+{
+    buildingInfo.GetHeight();
+    buildingInfo.GetEntranceFlats();
+    buildingInfo.GetEntrancesFlats();
+    Console.WriteLine($"Номер здания: {buildingInfo.Number}");
+    Console.WriteLine($"Высота здания: {buildingInfo.Height} м.");
+    Console.WriteLine($"Количество этажей: {buildingInfo.Floor}");
+    Console.WriteLine($"Количество квартир на 1 этаже: {buildingInfo.Flats}");
+    Console.WriteLine($"Количество подъездов в доме: {buildingInfo.Entrance}");
+    Console.WriteLine($"Количество квартир в 1 подъезде: {buildingInfo.EntranceFlats}");
+    Console.WriteLine($"Количество квартир во всём доме: {buildingInfo.EntrancesFlats}");
+    Console.WriteLine();
+}
+
+Console.ReadKey(true);
