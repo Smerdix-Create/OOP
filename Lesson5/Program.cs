7572b6663badd9a395a1531906d3178112fa4fb3
Nikita <Smerdix@mail.ru>
2022-07-11 08:27:33 +0400

    Работа с рациональными числами

---
+++ Lesson5/Program.cs
@@ -0,0 +1,18 @@
+﻿using Lesson5;
+
+var a = new RationalNumbers(-4, 7);
+var b = new RationalNumbers(5, 8);
+
+var c = a + b;
+
+
+/*
+ https://tutomath.ru/6-klass/slozhenie-racionalnyx-chisel-pravila-i-primery.html
+https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D1%87%D0%B8%D1%81%D0%BB%D0%BE
+ */
+
+Console.WriteLine(c.Numerator);
+Console.WriteLine("-");
+Console.WriteLine(c.Denominator);
+
+Console.ReadKey();
---
+++ Lesson5/RationalNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson5
+{
+    class RationalNumbers
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public RationalNumbers (int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public static RationalNumbers operator +(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+                return new RationalNumbers((a.Numerator + b.Numerator) /a.Denominator, a.Numerator);
+            }
+            else
+            {
+                return new RationalNumbers((a.Numerator * b.Denominator + b.Numerator * a.Denominator), (a.Denominator * b.Denominator));
+            }
+        }
+
+    }
+}
+
