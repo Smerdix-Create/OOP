6cbba0a7451fa61f9a7b4b6c1c9737965d930e56
Nikita <Smerdix@mail.ru>
2022-07-12 09:18:59 +0400

    Добавил методы вычисления, сравнения и т.д. для рациональных числе

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,15 +1,23 @@
 ﻿using Lesson5;
 
-var a = new RationalNumbers(-4, 7);
-var b = new RationalNumbers(5, 8);
-
-var c = a + b;
-
-
+var a = new RationalNumbers(3, 5);
+var b = new RationalNumbers(0, 0);
 /*
- https://tutomath.ru/6-klass/slozhenie-racionalnyx-chisel-pravila-i-primery.html
-https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D1%87%D0%B8%D1%81%D0%BB%D0%BE
- */
+Console.WriteLine(a != b);
+Console.WriteLine(a <= b);
+
+b++;
+Console.WriteLine($"{b.Numerator}, {b.Denominator}");
+Console.WriteLine();*/
+
+if (a.Denominator == 0 || b.Denominator == 0)
+{
+    Console.WriteLine("Знаменатель не может быть нулем");
+    return;
+}
+
+var c = a / b;
+c.GetAbbreviatedNumber();
 
 Console.WriteLine(c.Numerator);
 Console.WriteLine("-");
--- Lesson5/RationalNumbers.cs
+++ Lesson5/RationalNumbers.cs
@@ -2,8 +2,14 @@
 {
     class RationalNumbers
     {
-        public int Numerator { get; set; }
-        public int Denominator { get; set; }
+        /// <summary>
+        /// Числитель
+        /// </summary>
+        public int Numerator { get; private set; }
+        /// <summary>
+        /// Знаменатель
+        /// </summary>
+        public int Denominator { get; private set; }
 
         public RationalNumbers (int numerator, int denominator)
         {
@@ -11,16 +17,217 @@
             Denominator = denominator;
         }
 
+        /// <summary>
+        /// Вычисление суммы
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
         public static RationalNumbers operator +(RationalNumbers a, RationalNumbers b)
         {
             if (a.Denominator == b.Denominator)
             {
-                return new RationalNumbers((a.Numerator + b.Numerator) /a.Denominator, a.Numerator);
+                return new RationalNumbers(a.Numerator + b.Numerator, a.Denominator);
             }
             else
             {
-                return new RationalNumbers((a.Numerator * b.Denominator + b.Numerator * a.Denominator), (a.Denominator * b.Denominator));
+                return new RationalNumbers(a.Numerator * b.Denominator + b.Numerator * a.Denominator, a.Denominator * b.Denominator);
             }
+        }
+
+        /// <summary>
+        /// Вычисление разности
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static RationalNumbers operator -(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+
+                return new RationalNumbers(a.Numerator - b.Numerator, a.Denominator);
+            }
+            else
+            {
+                return new RationalNumbers(a.Numerator * b.Denominator - b.Numerator * a.Denominator, a.Denominator * b.Denominator);
+            }
+        }
+
+        /// <summary>
+        /// Вычисление произведения
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static RationalNumbers operator *(RationalNumbers a, RationalNumbers b)
+        {
+            return new RationalNumbers(a.Numerator * b.Numerator, a.Denominator * b.Denominator); 
+        }
+
+
+        /// <summary>
+        /// Вычисление частного
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static RationalNumbers operator /(RationalNumbers a, RationalNumbers b)
+        {
+            return new RationalNumbers(a.Numerator * b.Denominator, a.Denominator * b.Numerator);
+        }
+
+        /// <summary>
+        /// Инкрементация дроби
+        /// </summary>
+        /// <param name="a">Дробь</param>
+        /// <returns></returns>
+        public static RationalNumbers operator ++(RationalNumbers a)
+        {
+            a.Numerator++;
+            a.Denominator++;
+            return a;
+        }
+
+        /// <summary>
+        /// Декрементация дроби
+        /// </summary>
+        /// <param name="a">Дробь</param>
+        /// <returns></returns>
+        public static RationalNumbers operator --(RationalNumbers a)
+        {
+            a.Numerator--;
+            a.Denominator--;
+            return a;
+        }
+
+        /// <summary>
+        /// Проверка равенства дроби по полям
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static bool operator ==(RationalNumbers a, RationalNumbers b)
+        {
+            return (a.Numerator == b.Numerator && a.Denominator == b.Denominator);
+        }
+
+        /// <summary>
+        /// Проверка неравенства дроби по полям
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static bool operator !=(RationalNumbers a, RationalNumbers b)
+        {
+            return (a.Numerator != b.Numerator || a.Denominator != b.Denominator);
+        }
+
+        /// <summary>
+        /// Сравнение дробей. Больше
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static bool operator >(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+
+                return (a.Numerator > b.Numerator);
+            }
+            else
+            {
+                return (a.Denominator * b.Denominator / a.Denominator * a.Numerator > a.Denominator * b.Denominator / b.Denominator * b.Numerator);
+            }
+        }
+
+        /// <summary>
+        /// Сравнение дробей. Меньше
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static bool operator <(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+
+                return (a.Numerator < b.Numerator);
+            }
+            else
+            {
+                return (a.Denominator * b.Denominator / a.Denominator * a.Numerator < a.Denominator * b.Denominator / b.Denominator * b.Numerator);
+            }
+        }
+
+        /// <summary>
+        /// Сравнение дробей. Больше или равно
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static bool operator >=(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+
+                return (a.Numerator >= b.Numerator);
+            }
+            else
+            {
+                return (a.Denominator * b.Denominator / a.Denominator * a.Numerator >= a.Denominator * b.Denominator / b.Denominator * b.Numerator);
+            }
+        }
+
+        /// <summary>
+        /// Сравнение дробей. Меньше или равно
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static bool operator <=(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+
+                return (a.Numerator <= b.Numerator);
+            }
+            else
+            {
+                return (a.Denominator * b.Denominator / a.Denominator * a.Numerator <= a.Denominator * b.Denominator / b.Denominator * b.Numerator);
+            }
+        }
+
+        /// <summary>
+        /// Вычисление остатка от деления
+        /// </summary>
+        /// <param name="a">Первая дробь</param>
+        /// <param name="b">Вторая дробь</param>
+        /// <returns></returns>
+        public static double operator %(RationalNumbers a, RationalNumbers b)
+        {
+            int z = a.Numerator * b.Denominator;
+            int x = a.Denominator * b.Numerator;
+
+            return z - (z / x) * x;
+        }
+
+        /// <summary>
+        /// Получение сокращенной дроби
+        /// </summary>
+        public void GetAbbreviatedNumber()
+        {
+            for (int i = 2; i < this.Denominator; i++)
+            {
+                if (this.Numerator % i == 0 && this.Denominator % i == 0)
+                {
+                    this.Numerator = this.Numerator / i;
+                    this.Denominator = this.Denominator / i;
+                    i--;
+                }
+            }
+            return;
         }
 
     }
