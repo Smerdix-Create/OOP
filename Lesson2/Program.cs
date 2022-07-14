834600f43fab48ec95e4ccc04d34e695dd0e06d4
Nikita <Smerdix@mail.ru>
2022-07-14 15:25:47 +0400

    В классе BankAccount переопределил операторы == и !=. Также переопределил методы Equals, GetHashCode и ToString.

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -20,12 +20,13 @@ void GetAccount(BankAccount account)
 
 
 //Вторая реализация
-BankAccount[] accountNumbers = new BankAccount[5];
+BankAccount[] accountNumbers = new BankAccount[6];
 accountNumbers[0] = new(343454, TypeBankAccount.Credit);
 accountNumbers[1] = new(546456, TypeBankAccount.Accounting);
 accountNumbers[2] = new(3244, TypeBankAccount.Credit);
 accountNumbers[3] = new(23);
 accountNumbers[4] = new(TypeBankAccount.Deposit);
+accountNumbers[5] = new(343454, TypeBankAccount.Credit);
 
 accountNumbers[4].TransferMoney(accountNumbers[3], 23);
 
@@ -36,5 +37,13 @@ foreach (BankAccount bankAccount in accountNumbers)
     Console.WriteLine($"Баланс счета: {bankAccount.Balance}");
     Console.WriteLine();
 }
+
+
+Console.WriteLine(accountNumbers[0] == accountNumbers[5]);
+Console.WriteLine(accountNumbers[0] != accountNumbers[5]);
+Console.WriteLine(accountNumbers[0].Equals(accountNumbers[5]));
+Console.WriteLine(accountNumbers[0].GetHashCode());
+Console.WriteLine(accountNumbers[5].GetHashCode());
+Console.WriteLine(accountNumbers[0].ToString());
 
 Console.ReadKey(true);
